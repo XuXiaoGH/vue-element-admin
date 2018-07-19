3408db3f247b1f19b76b190f774003bfa1e5f47e
xuxiao <heixuxiao@163.com>
2018-07-19 23:54:24 +0800

    1、Test

--- src/views/tab/index.vue
+++ src/views/tab/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tab-container">
-    <el-tag>mounted times ：{{createdTimes}}</el-tag>
+    <el-tag>mounted times haha：{{createdTimes}}</el-tag>
     <el-alert style="width:200px;display:inline-block;vertical-align: middle;margin-left:30px;" title="Tab with keep-alive" type="success" :closable="false">
     </el-alert>
     <el-tabs style='margin-top:15px;' v-model="activeName" type="border-card">
